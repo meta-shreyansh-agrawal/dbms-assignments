0f0cd0484d918adc696fb8a05c44c417f2f108ce
Shreyansh Agrawal <shreyansh.agrawal@metacube.com>
2025-04-09 10:27:18 +0530

    modified: ProductDao

--- storefront/src/main/java/com/storefront/thirdassignment/ProductDao.java
+++ storefront/src/main/java/com/storefront/thirdassignment/ProductDao.java
@@ -11,10 +11,8 @@ public class ProductDao {
         int count = 0; 
         Connection connection = DatabaseConnector.connect();
         try {
-            connection.prepareStatement("SET SQL_SAFE_UPDATES = 0;"); 
-            String query = "WHERE id NOT IN ( SELECT DISTINCT product_id FROM order_items oi JOIN orders o ON oi.order_id = o.id JOIN users u ON o.user_id = u.id WHERE u.role = 'Shopper' AND o.order_time >= NOW() - INTERVAL 1 YEAR);"; 
+            String query = "DELETE FROM products WHERE id NOT IN ( SELECT DISTINCT product_id FROM order_items oi JOIN orders o ON oi.order_id = o.id JOIN users u ON o.user_id = u.id WHERE u.role = 'Shopper' AND o.order_time >= NOW() - INTERVAL 1 YEAR);"; 
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            connection.prepareStatement("SET SQL_SAFE_UPDATES = 1;"); 
             count = preparedStatement.executeUpdate(); 
             connection.close();
         } catch (SQLException e) {
